07de657b552c10883ba5e0f45f695e003bc210cb
ivnlee <a01793491@tec.mx>
2023-07-07 23:42:50 -0600

    layout refresh

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import streamlit as st
 import json
 import requests
+import time
 from newspaper import Article
 
+
 # Page layout
-st.title("Article Summarizer app")
-st.markdown("#### This app summarizes web articles using HuggingFace's transformers library")
+c1, c2 = st.columns([0.32, 2])
 
-# inputs: 
+with c1:
+     st.image("images/newspaper.png", width=85)
+
+with c2:
+    st.title("FastNews Article Summarizer")
+
+st.markdown("**Generate summaries of online articles using abstractive summarization with Google's PEGASUS model.**")
+
+if not "valid_inputs_received" in st.session_state:
+    st.session_state["valid_inputs_received"] = False
+    st.stop()
+
+# Sidebar content
+st.sidebar.subheader("About the app")
+st.sidebar.info("This app uses 🤗HuggingFace's [google/pegasus-cnn_dailymail](https://huggingface.co/google/pegasus-cnn_dailymail) model.\
+                 \nYou can find the source code [here](https://github.com/ivnlee/streamlit-text-summarizer)")
+st.sidebar.write("\n\n")
+st.sidebar.markdown("**Get a free API key from HuggingFace:**")
+st.sidebar.markdown("* Create a [free account](https://huggingface.co/join) or [login](https://huggingface.co/login)")
+st.sidebar.markdown("* Go to **Settings** and then **Access Tokens**")
+st.sidebar.markdown("* Create a new Token (select 'read' role)")
+st.sidebar.markdown("* Paste your API key in the text box")
+st.sidebar.divider()
+st.sidebar.markdown("Please make sure your article is in English and is not behind a paywall.")
+st.sidebar.write("\n\n")
+st.sidebar.info("Created by [Ivan Lee](https://ivan-lee.medium.com/) using [Streamlit](https://streamlit.io/)🎈.")
+
+
+# Inputs 
 st.subheader("Enter the URL of the article you want to summarize")
-default_url = "https://thehackernews.com/2023/07/mexico-based-hacker-targets-global.html"
+default_url = "https://www.reuters.com/technology/un-recruits-robots-strive-meet-global-development-goals-2023-07-05/"
 url = st.text_input("URL:", default_url)
-st.divider()
-
 
 headers_ = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
@@ -21,33 +48,49 @@ headers_ = {
 article_url = url
 session = requests.Session()
 
-response_ = session.get(article_url, headers=headers_, timeout=10)
-
-if response_.status_code == 200:
-    article = Article(article_url)
-    article.download()
-    article.parse()
-
-    title = article.title
-    text = article.text
-
-else:
-    st.write("Error while fetching the article, please check URL...")
+try:
+    response_ = session.get(article_url, headers=headers_, timeout=10)
+    
+    if response_.status_code == 200:
+        article = Article(article_url)
+        article.download()
+        article.parse()
+        
+        title = article.title
+        text = article.text
+        
+    else:
+        st.write("Failed to fetch article, please check URL...")
+except Exception as e:
+    st.write(f"Error occurred while fetching article: {e}")
 
 # HuggingFace API KEY
 API_KEY = st.text_input("Enter your HuggingFace API key", type="password")
 
 # HuggingFace API inference URL.
 API_URL = "https://api-inference.huggingface.co/models/google/pegasus-cnn_dailymail"
+
 headers = {"Authorization": f"Bearer {API_KEY}"}
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-  
-output = query({"inputs": text, })
 
-st.divider()
-st.subheader("Summary")
-st.write(title)
-st.write(output)
+submit_button = st.button("Submit")
+
+# Summary
+if submit_button:
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    st.session_state["valid_inputs_received"] = True
+    output = query({"inputs": text, })
+    summary = output[0]['summary_text'].replace('<n>', " ")
+
+    with st.spinner('Doing AI magic, please wait...'):
+        time.sleep(5)
+    st.success('Done!')
+
+    st.divider()
+    st.subheader("Summary")
+    st.write("Your article: ", title)
+    st.write(summary)
